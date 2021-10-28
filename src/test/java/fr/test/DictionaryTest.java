641c5c4c0ec1956da930a5c36e2124f1e89cd66e
Celina-Hadjara <hadjaracelina3@gmail.com>
2021-10-28 15:58:33 +0200

    Rendu Final.

--- src/test/java/fr/test/DictionaryTest.java
+++ src/test/java/fr/test/DictionaryTest.java
@@ -5,7 +5,6 @@ import fr.main.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 
 import java.util.ArrayList;
